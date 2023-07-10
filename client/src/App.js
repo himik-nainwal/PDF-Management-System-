d95aa4731bc6ad21dc68367957b06261b55b286b
Himik Nainwal <himithnainwal@gmail.com>
2023-07-10 11:21:32 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import * as URL from "./URL";
 import axios from 'axios';
@@ -9,64 +9,71 @@ import Dashboard from './Dashboard/Dashboard';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import './Spinner.css'; // Import the spinner CSS file
+
 function App() {
-  const [user, setUser] = useState();
-  const [token, setToken]=useState("");
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   useEffect(() => {
     setToken(localStorage.getItem('token'));
+
     async function fetchUserDetails() {
       try {
-        const response = await axios.post(URL.API_URL+'data/dataofusers', null, {
+        setLoading(true);
+        const response = await axios.post(URL.API_URL + 'data/dataofusers', null, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-          console.log("aaaaa",response);
+        console.log("aaaaa", response);
         if (response.data.success) {
           const userDetails = response.data.user;
           setUser(userDetails);
         } else {
           console.log(response.data.message);
           setUser(null);
+          navigate('/');
         }
       } catch (error) {
         console.error(error);
         setUser(null);
+        navigate('/');
+      } finally {
+        setLoading(false);
       }
     }
-    if (!token) {
-      fetchUserDetails(token);
-    } else if (
-      window.location.pathname !=="/" && 
-      window.location.pathname !=="/forgotpass") {
-      window.location.href ="/";
-    console.log("aaaaa")}
-  }, []);
-  console.log("This is ",user);
+
+    if (token) {
+      fetchUserDetails();
+    } else {
+      setUser(null);
+      navigate('/');
+    }
+  }, [token, navigate]);
+
   return (
     <>
-    <Routes>
-    {!localStorage.getItem("token") && (
-          <>
-            <Route path="/" element={<Login />} />
-            <Route path="forgotpass" element ={<ForgotPass />} />
-          </>
-        )}
-       {token && (<>
-       <Route path="/dashboard" element={<Dashboard/>}/>
-       </>
-       )}
-        <Route
-									path='*'
-									element={
-										<h1 className='flex justify-center font-bold text-slate-800'>
-											404 Page not Available
-										</h1>
-									}
-								/>
-
-    </Routes>
-    <ToastContainer />
+      {loading ? (
+        // Show spinner while loading
+        <div className="flex justify-center items-center h-screen">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        // Show routes
+        <Routes>
+          <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Login />} />
+          <Route path="forgotpass" element={<ForgotPass />} />
+          <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+          <Route
+            path='*'
+            element={<Navigate to="/" replace />}
+          />
+        </Routes>
+      )}
+      <ToastContainer />
     </>
   );
 }
