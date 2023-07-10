572be66a5990aa21f114acc5ac83f2dbfe690b38
Himik Nainwal <himithnainwal@gmail.com>
2023-07-10 13:14:38 +0530

    Netlify Fix

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 module.exports = {
   allowedOrigins: [
-    "http://localhost:3000/"
+    "http://localhost:3000/",
+    "https://guileless-gumdrop-7419e1.netlify.app/dashboard",
+    "https://guileless-gumdrop-7419e1.netlify.app"
   ],
   SERVER_PORT: process.env.PORT || 3000,
   SERVER_DB_URI: process.env.DB_URL,
